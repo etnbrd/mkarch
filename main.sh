d7e98f1949cd75b72bb2bd0b96fcbae092e51f66
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2013-10-18 19:50:51 +0200

    fix some errors

--- main.sh
+++ main.sh
@@ -69,27 +69,27 @@ MKBASE=true;
 # quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/utils.sh &&
 quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/mkdisk.sh &&
 quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/mkpart.sh &&
-quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/mkbase.sh
-Error $? "$ER ressource not available"
+quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/mkbase.sh;
+Error $? "$ER ressource not available";
 
 # wuiet wget https://raw.github.com/gravitezero/mkarch/master/utils.sh -O - | source;
 
-Ask "Host ?" HOST
-Ask "Make disk ?" MKDISK
-Ask "Make part ?" MKPART
-Ask "Make base ?" MKBASE
+Ask "Host ?" HOST;
+Ask "Make disk ?" MKDISK;
+Ask "Make part ?" MKPART;
+Ask "Make base ?" MKBASE;
 
-quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/hosts/$HOST/init.sh
-Error $? "$ER hosts/$HOST/init.sh doesn't exist"
+quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/hosts/$HOST/init.sh;
+Error $? "$ER hosts/$HOST/init.sh doesn't exist";
 
 quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/hosts/$HOST/init.sh -O - | source;
 
 if [[ MKDISK = true ]]; then
-  quiet wget https://raw.github.com/gravitezero/mkarch/master/mkdisk.sh -O - | sh
+  quiet wget https://raw.github.com/gravitezero/mkarch/master/mkdisk.sh -O - | sh;
 fi
 if [[ MKPART = true ]]; then
-  quiet wget https://raw.github.com/gravitezero/mkarch/master/mkpart.sh -O - | sh
+  quiet wget https://raw.github.com/gravitezero/mkarch/master/mkpart.sh -O - | sh;
 fi
 if [[ MKBASE = true ]]; then
-  quiet wget https://raw.github.com/gravitezero/mkarch/master/mkbase.sh -O - | sh
+  quiet wget https://raw.github.com/gravitezero/mkarch/master/mkbase.sh -O - | sh;
 fi
