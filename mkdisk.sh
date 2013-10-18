a4b4c55bcbf7c971db0104c13c2dabc0175f9087
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2013-10-18 10:52:24 +0200

    colors

--- mkdisk.sh
+++ mkdisk.sh
@@ -8,7 +8,7 @@
 #####################################################
 
 Init() {
-  ASK=false
+  ASK=true
   DISK_DEVICE=/dev/vda
   ROOT_SIZE=5G
   HOME_SIZE=10G
@@ -20,13 +20,39 @@ Init() {
 # UTILS                                             #
 #####################################################
 
+TC='\e['
+ 
+CLR_LINE_START="${TC}1K"
+CLR_LINE_END="${TC}K"
+CLR_LINE="${TC}2K"
+ 
+Bold="${TC}1m" # Bold text only, keep colors
+Undr="${TC}4m" # Underline text only, keep colors
+Inv="${TC}7m" # Inverse: swap background and foreground colors
+Reg="${TC}22;24m" # Regular text only, keep colors
+RegF="${TC}39m" # Regular foreground coloring
+RegB="${TC}49m" # Regular background coloring
+Rst="${TC}0m" # Reset all coloring and style
+ 
+# Basic                     High Intensity              Background                    High Intensity Background
+Black="${TC}30m";           IBlack="${TC}90m";          OnBlack="${TC}40m";           OnIBlack="${TC}100m";
+Red="${TC}31m";             IRed="${TC}91m";            OnRed="${TC}41m";             OnIRed="${TC}101m";
+Green="${TC}32m";           IGreen="${TC}92m";          OnGreen="${TC}42m";           OnIGreen="${TC}102m";
+Yellow="${TC}33m";          IYellow="${TC}93m";         OnYellow="${TC}43m";          OnIYellow="${TC}103m";
+Blue="${TC}34m";            IBlue="${TC}94m";           OnBlue="${TC}44m";            OnIBlue="${TC}104m";
+Purple="${TC}35m";          IPurple="${TC}95m";         OnPurple="${TC}45m";          OnIPurple="${TC}105m";
+Cyan="${TC}36m";            ICyan="${TC}96m";           OnCyan="${TC}46m";            OnICyan="${TC}106m";
+White="${TC}37m";           IWhite="${TC}97m";          OnWhite="${TC}47m";           OnIWhite="${TC}107m";
+
+PROMPT="${Cyan}>${Purple}>${Rst}";
+
 Ask() {
   while true
   do
     if [[ -z ${!2} ]]; then
-      echo -n "$1 ";
+      echo -n "$PROMPT $1 ";
     elif [[ $ASK = true ]]; then
-      echo -n "$1 [${!2}] ";
+      echo -n "$PROMPT $1 [${!2}] ";
     else
       break;
     fi
@@ -78,7 +104,7 @@ do
   echo -e "   home \t$HOME_SIZE"
   echo ""
 
-  while true; do
+  while [[ $ASK = true ]]; do
       echo "Good ? [yes/no]"
       read yn
       case $yn in
