f33c32892f97ac2a8fd9dd7dd0c75cfec189f83a
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2013-10-19 17:18:35 +0200

    fix errors

--- main.sh
+++ main.sh
@@ -18,9 +18,9 @@ MKBASE=true;
 
 source <(wget https://raw.github.com/gravitezero/mkarch/master/utils.sh -qO -);
 
-quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/mkdisk.sh &&
-quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/mkpart.sh &&
-quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/mkbase.sh;
+wget -q --spider https://raw.github.com/gravitezero/mkarch/master/mkdisk.sh &&
+wget -q --spider https://raw.github.com/gravitezero/mkarch/master/mkpart.sh &&
+wget -q --spider https://raw.github.com/gravitezero/mkarch/master/mkbase.sh;
 Error $? "$ER ressource not available" "$IF ressources available";
 
 Ask "Host ?" HOSTNAME
@@ -28,25 +28,21 @@ Ask "Make disk ?" MKDISK
 Ask "Make part ?" MKPART
 Ask "Make base ?" MKBASE
 
-quiet wget --spider https://raw.github.com/gravitezero/mkarch/master/hosts/$HOSTNAME/init.sh;
+wget -q --spider https://raw.github.com/gravitezero/mkarch/master/hosts/$HOSTNAME/init.sh;
 Error $? "$ER hosts/${HOSTNAME}/init.sh doesn't exist";
 
-mkdir -p tmp;
-wget https://raw.github.com/gravitezero/mkarch/master/hosts/$HOSTNAME/init.sh -O - > tmp/init.sh 2> /dev/null;
-chmod +x ./tmp/init.sh;
-
-source ./tmp/init.sh;
+source <(wget https://raw.github.com/gravitezero/mkarch/master/hosts/$HOSTNAME/init.sh -qO -);
 Init;
 
 if [[ $MKDISK = true ]]; then
   echo -e "\n>$IF Making disk"
-  source <(wget https://raw.github.com/gravitezero/mkarch/master/mkdisk.sh -O - 2> /dev/null);
+  source <(wget https://raw.github.com/gravitezero/mkarch/master/mkdisk.sh -qO -);
 fi
 if [[ $MKPART = true ]]; then
   echo -e "\n>$IF Making part"
-  source <(wget https://raw.github.com/gravitezero/mkarch/master/mkpart.sh -O - 2> /dev/null);
+  source <(wget https://raw.github.com/gravitezero/mkarch/master/mkpart.sh -qO -);
 fi
 if [[ $MKBASE = true ]]; then
   echo -e "\n>$IF Making base"
-  source <(wget https://raw.github.com/gravitezero/mkarch/master/mkbase.sh -O - 2> /dev/null);
+  source <(wget https://raw.github.com/gravitezero/mkarch/master/mkbase.sh -qO -);
 fi
