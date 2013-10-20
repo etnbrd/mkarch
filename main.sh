b19242f2170b821581b8883042847bab205cc382
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2013-10-20 19:39:46 +0200

    testing installation process

--- main.sh
+++ main.sh
@@ -10,10 +10,10 @@
 # MAIN                                              #
 #####################################################
 
-ASK=true;
+ASK=false;
 HOSTNAME=arch-srv;
-MKDISK=false;
-MKPART=false;
+MKDISK=true;
+MKPART=true;
 MKBASE=true;
 
 SOURCE="https://raw.github.com/gravitezero/mkarch/master";
