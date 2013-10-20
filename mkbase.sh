1636b8967c443761d5173eb947c095ce77a41808
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2013-10-20 17:30:21 +0200

    testing installation process

--- mkbase.sh
+++ mkbase.sh
@@ -79,12 +79,13 @@ chrootsh locale-gen
 Error $? "$ER Failed to generate locales" "$IF locales generated"
 
 wget ${SOURCE}/hosts/$HOSTNAME/vconsole.conf -qO - > /mnt/etc/vconsole.conf
-Error $? "$ER Failed to setup vconsole" "$IF vconsole\n\t${BIYel}`cat /mnt/etc/vconsole.conf | sed s_^_\\\t_`${Rst}"
+Error $? "$ER Failed to setup vconsole" "$IF vconsole\n${BIYel}`cat /mnt/etc/vconsole.conf | sed s_^_\\\t_`${Rst}"
 
 chrootsh mkinitcpio -p linux
 Error $? "$ER Failed to make initramfs" "$IF intiramfs created"
 
 # TODO get the complete pacman.conf
+chrootsh echo $arch
 chrootsh echo "[archlinuxfr]\n  SigLevel = Never\n  Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf;
 
 chrootsh pacman -Sy yaourt;
