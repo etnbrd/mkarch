d6c5072f755ddf7baed4573fae8f03c5d203f60b
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2013-10-20 15:45:52 +0200

    testing installation process

--- mkbase.sh
+++ mkbase.sh
@@ -63,32 +63,31 @@ done
 
 # echo "${BIGre}>>${BIWhi} Success${Rst}, we made it to the ARCH-CHROUT, time to unpack salt, and let it roll :)";
 
-chrootsh rm /etc/localtime
-chrootsh ln -s /usr/share/zoneinfo/$LOCALZONE /etc/localtime
-Error $? "$ER Failed to setup localtime" "$IF localtime \t${BIYel}$LOCALZONE${Rst}"
-
 # arch-chroot /mnt << EOF
 chrootsh echo $HOSTNAME > /etc/hostname;
 Error $? "$ER Failed to setup hostname" "$IF hostname \t${BIYel}`cat /mnt/etc/hostname`${Rst}"
 
+chrootsh rm /etc/localtime
+chrootsh ln -s /usr/share/zoneinfo/$LOCALZONE /etc/localtime
+Error $? "$ER Failed to setup localtime" "$IF localtime \t${BIYel}$LOCALZONE${Rst}"
 
-# chrootsh "wget ${SOURCE}/hosts/$HOSTNAME/locale.gen -qO - > /etc/locale.gen"
-# Error $? "$ER Failed to setup locales" "$IF locales \t{BIYel}`cat /mnt/etc/locale.gen`${Rst}"
+chrootsh wget ${SOURCE}/hosts/$HOSTNAME/locale.gen -qO - > /etc/locale.gen
+Error $? "$ER Failed to setup locales" "$IF locales \t{BIYel}`cat /mnt/etc/locale.gen`${Rst}"
 
-# chrootsh locale-gen
-# Error $? "$ER Failed to generate locales" "$IF locales generated"
+chrootsh locale-gen
+Error $? "$ER Failed to generate locales" "$IF locales generated"
 
-# chrootsh "wget ${SOURCE}/hosts/$HOSTNAME/vconsole.conf -qO - > /etc/vconsole.conf"
-# Error $? "$ER Failed to setup vconsole" "$IF locales: \t{BIYel}`cat /mnt/etc/vconsole.conf`${Rst}"
+chrootsh wget ${SOURCE}/hosts/$HOSTNAME/vconsole.conf -qO - > /etc/vconsole.conf
+Error $? "$ER Failed to setup vconsole" "$IF locales: \t{BIYel}`cat /mnt/etc/vconsole.conf`${Rst}"
 
-# chrootsh "mkinitcpio -p linux"
-# Error $? "$ER Failed to make initramfs" "$IF intiramfs created"
+chrootsh mkinitcpio -p linux
+Error $? "$ER Failed to make initramfs" "$IF intiramfs created"
 
-# chrootsh echo "[archlinuxfr]
-#               SigLevel = Never
-#               Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf;
+chrootsh echo "[archlinuxfr]
+              SigLevel = Never
+              Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf;
 
-# chrootsh pacman -Sy yaourt;
+chrootsh pacman -Sy yaourt;
 
 # TODO do the bootloader
 # TODO do the root password
