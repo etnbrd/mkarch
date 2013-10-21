5859fa7e5d66b9b4b44930c5566192d652ab143e
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2013-10-21 13:03:57 +0200

    testing installation process

--- mkdisk.sh
+++ mkdisk.sh
@@ -77,7 +77,7 @@ quiet umount /mnt/{home,};
 quiet sgdisk -o $DISK_DEVICE;                                  # Clear partition
 Error $? "$ER Couldn't Clear Partition" "$IF Disk erased successfully";
 
-quiet sgdisk -n 1:0:1007K -t 1:0xEF02 -c 1:'boot' $DISK_DEVICE;
+sgdisk -n 1:0:1007K -t 1:0xEF02 -c 1:'boot' $DISK_DEVICE;
 Error $? "$ER Couldn't create boot partition" "$IF Boot partition created"
 
 quiet sgdisk -n 2:1007K:+$ROOT_SIZE -c 2:'root' $DISK_DEVICE;      # Create root partition
