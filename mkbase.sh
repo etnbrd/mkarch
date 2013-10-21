0e71dbba29ca388273152d16ce028411fdaee716
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2013-10-21 13:39:20 +0200

    testing installation process

--- mkbase.sh
+++ mkbase.sh
@@ -62,7 +62,7 @@ Error $? "$ER ${BWhi}pacstrap${Rst} failed" "$IF pacstrap successful";
 
 genfstab -U -p /mnt >> /mnt/etc/fstab;
 
-chrootsh "echo -e '$ROOT_PWD\n$ROOT_PWD' | passwd"
+chrootsh echo -e '$ROOT_PWD\n$ROOT_PWD' | passwd
 Error $? "$ER Failed to set root passwd" "$IF root passwd"
 
 chrootsh "echo $HOSTNAME > /etc/hostname"
