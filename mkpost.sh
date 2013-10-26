af5c446844e9021b221c11f56a0b2ce809955349
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2013-10-26 16:33:47 +0200

    testing installation process

--- mkpost.sh
+++ mkpost.sh
@@ -4,19 +4,19 @@
 # This script install the salted system
 
 # The one-liner :)
-# zsh <(wget https://raw.github.com/gravitezero/mkarch/master/mkpost.sh -qO -);
+# zsh <(curl -s https://raw.github.com/gravitezero/mkarch/master/mkpost.sh);
 
 #####################################################
 # COMMANDS                                          #
 #####################################################
 
 SOURCE="https://raw.github.com/gravitezero/mkarch/master";
-HOSTNAME = `cat /etc/hostname`;
+HOSTNAME=`cat /etc/hostname`;
 
-source <(wget ${SOURCE}/utils.sh -qO -);
+source <(curl -s ${SOURCE}/utils.sh);
 
 # TODO get the complete pacman.conf
-curl ${SOURCE}/hosts/$HOSTNAME/archlinuxfr.repo >> /mnt/etc/pacman.conf
+curl -s ${SOURCE}/hosts/$HOSTNAME/archlinuxfr.repo >> /mnt/etc/pacman.conf
 # wget ${SOURCE}/hosts/$HOSTNAME/archlinuxfr.repo -qO - >> /mnt/etc/pacman.conf
 
 chrootsh pacman -Sy --noconfirm yaourt &&
@@ -26,7 +26,7 @@ Error $? "$ER Failed to install yaourt and salt" "$IF yaourt and salt installed"
 # TODO states should be stored in home
 # TODO find another way to get states
 rm -rf /srv/salt;
-wget -q https://github.com/gravitezero/mkarch/archive/master.tar.gz;
+curl -s -o master.tar.gz https://github.com/gravitezero/mkarch/archive/master.tar.gz;
 quiet tar xzvf master.tar.gz;
 mv mkarch-master/hosts/$HOSTNAME/salt /srv;
 rm -rf master.tar.gz mkarch-master;
