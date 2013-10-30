591e5ceeb676ad71ae191bfb6df5b95b63d7f39e
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2013-10-30 19:30:57 +0100

    testing installation process

--- mkpost.sh
+++ mkpost.sh
@@ -14,7 +14,7 @@
 SOURCE="https://raw.github.com/gravitezero/mkarch/master";
 HOSTNAME=`cat /etc/hostname`;
 
-source $(curl -s ${SOURCE}/utils.sh);
+curl -s ${SOURCE}/utils.sh | source /dev/stdin;
 
 # TODO get the complete pacman.conf
 curl -s ${SOURCE}/hosts/$HOSTNAME/archlinuxfr.repo >> /mnt/etc/pacman.conf
